a9724b0f6cd1ba74ce5a7d4a4123245385080cf5
Tung Pho <ductungpho1005@gmail.com>
2025-03-02 22:07:56 +0700

    new update

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Khi deploy, thay "*" bằng URL frontend trên Vercel
+    origin: "https://test-deploy-frontend-orpin.vercel.app/", // Khi deploy, thay "*" bằng URL frontend trên Vercel
     methods: ["GET", "POST"],
   },
 });
